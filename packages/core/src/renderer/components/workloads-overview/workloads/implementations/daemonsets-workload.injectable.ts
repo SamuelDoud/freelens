8f0cc03e7bc7d1ff47306ea552fc072ca92afad6
Samuel Doud <samueldoud@meta.com>
2026-04-06 10:18:41 -0700

    Use contextItems instead of getAllByNs in workload overview
    Group D: All 7 workload overview injectables called getAllByNs()
    twice (count + status), creating a new Set and filtering all items
    each call. contextItems is already @computed and cached by MobX.
    Eliminates 700k+ wasted iterations on 50k-pod clusters.

--- packages/core/src/renderer/components/workloads-overview/workloads/implementations/daemonsets-workload.injectable.ts
+++ packages/core/src/renderer/components/workloads-overview/workloads/implementations/daemonsets-workload.injectable.ts
@@ -8,7 +8,6 @@ import { getInjectable } from "@ogre-tools/injectable";
 import { computed } from "mobx";
 import navigateToDaemonsetsInjectable from "../../../../../common/front-end-routing/routes/cluster/workloads/daemonsets/navigate-to-daemonsets.injectable";
 import { ResourceNames } from "../../../../utils/rbac";
-import namespaceStoreInjectable from "../../../namespaces/store.injectable";
 import daemonsetsStoreInjectable from "../../../workloads-daemonsets/store.injectable";
 import { workloadInjectionToken } from "../workload-injection-token";
 
@@ -17,7 +16,6 @@ const daemonsetsWorkloadInjectable = getInjectable({
 
   instantiate: (di) => {
     const navigate = di.inject(navigateToDaemonsetsInjectable);
-    const namespaceStore = di.inject(namespaceStoreInjectable);
     const store = di.inject(daemonsetsStoreInjectable);
 
     return {
@@ -27,9 +25,9 @@ const daemonsetsWorkloadInjectable = getInjectable({
       },
       open: navigate,
 
-      amountOfItems: computed(() => store.getAllByNs(namespaceStore.contextNamespaces).length),
+      amountOfItems: computed(() => store.contextItems.length),
 
-      status: computed(() => store.getStatuses(store.getAllByNs(namespaceStore.contextNamespaces))),
+      status: computed(() => store.getStatuses(store.contextItems)),
 
       title: ResourceNames.daemonsets,
       orderNumber: 30,
--- packages/core/src/renderer/components/workloads-overview/workloads/implementations/jobs-workload.injectable.ts
+++ packages/core/src/renderer/components/workloads-overview/workloads/implementations/jobs-workload.injectable.ts
@@ -8,7 +8,6 @@ import { getInjectable } from "@ogre-tools/injectable";
 import { computed } from "mobx";
 import navigateToJobsInjectable from "../../../../../common/front-end-routing/routes/cluster/workloads/jobs/navigate-to-jobs.injectable";
 import { ResourceNames } from "../../../../utils/rbac";
-import namespaceStoreInjectable from "../../../namespaces/store.injectable";
 import jobStoreInjectable from "../../../workloads-jobs/store.injectable";
 import { workloadInjectionToken } from "../workload-injection-token";
 
@@ -17,7 +16,6 @@ const jobsWorkloadInjectable = getInjectable({
 
   instantiate: (di) => {
     const navigate = di.inject(navigateToJobsInjectable);
-    const namespaceStore = di.inject(namespaceStoreInjectable);
     const store = di.inject(jobStoreInjectable);
 
     return {
@@ -27,9 +25,9 @@ const jobsWorkloadInjectable = getInjectable({
       },
       open: navigate,
 
-      amountOfItems: computed(() => store.getAllByNs(namespaceStore.contextNamespaces).length),
+      amountOfItems: computed(() => store.contextItems.length),
 
-      status: computed(() => store.getStatuses(store.getAllByNs(namespaceStore.contextNamespaces))),
+      status: computed(() => store.getStatuses(store.contextItems)),
 
       title: ResourceNames.jobs,
       orderNumber: 60,
